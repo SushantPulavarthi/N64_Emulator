57d1f82de608b7eb00f1cb45f490c0c1db635e64
Sushant <sushant.pulavarthi09@gmail.com>
2024-08-17 19:36:55 +0100

    Impl: Reading TLBEntries

--- src/cpu.zig
+++ src/cpu.zig
@@ -8,6 +8,13 @@ const Cp0 = @import("cp0.zig").Cp0;
 const Cp1 = @import("cp1.zig").Cp1;
 const Instructions = @import("instructions.zig");
 
+const Entry_lo = packed struct {
+    pfn: u20,
+    c: u3,
+    d: u1,
+    v: u1,
+};
+
 const TLBEntry = packed struct {
     pagemask: packed struct {
         mask: u12,
@@ -15,21 +22,11 @@ const TLBEntry = packed struct {
     entry_hi: packed struct {
         r: u2,
         vpn2: u27,
-        g: u1,
+        g: bool,
         asid: u8,
     },
-    entry_lo0: packed struct {
-        pfn: u20,
-        c: u3,
-        d: u1,
-        v: u1,
-    },
-    entry_lo1: packed struct {
-        pfn: u20,
-        c: u3,
-        d: u1,
-        v: u1,
-    },
+    entry_lo0: Entry_lo,
+    entry_lo1: Entry_lo,
 };
 
 // 64 bit MIPS r4300i chip
@@ -96,62 +93,69 @@ pub const Cpu = struct {
 
     // Memory accesses by main CPU, instruction fetched and load/store instructions use virtual addresses
     pub fn virtualToPhysical(self: *Cpu, address: u64) u64 {
-        _ = self;
         // 32 Bit mode address translation
         // const offset =
 
         return switch (address & 0xFFFF_FFFF) {
-            // KUSEG TLB
-            0x0000_0000...0x7FFF_FFFF => undefined,
-            // KSEG0 Direct Map, cache
-            0x8000_0000...0x9FFF_FFFF => address - 0x8000_0000,
-            // KSEG1 Direct Map, non cache
-            0xA000_0000...0xBFFF_FFFF => address - 0xA000_0000,
-            // KSSEG TLB
-            0xC000_0000...0xDFFF_FFFF => undefined,
-            // KSEG3 TLB
-            0xE000_0000...0xFFFF_FFFF => undefined,
+            // // KSEG0 Direct Map, cache
+            // 0x8000_0000...0x9FFF_FFFF => address - 0x8000_0000,
+            // // KSEG1 Direct Map, non cache
+            // 0xA000_0000...0xBFFF_FFFF => address - 0xA000_0000,
+            // // KUSEG TLB
+            // 0x0000_0000...0x7FFF_FFFF => tlbTranslate(self, address),
+            // // KSSEG TLB
+            // 0xC000_0000...0xDFFF_FFFF => tlbTranslate(self, address),
+            // // KSEG3 TLB
+            // 0xE000_0000...0xFFFF_FFFF => tlbTranslate(self, address),
+            0x8000_0000...0xBFFF_FFFF => address & 0x1FFF_FFFF,
+            0x0000_0000...0x7FFF_FFFF, 0xC000_0000...0xFFFF_FFFF => tlbTranslate(self, address),
             else => panic("Unrecognized Virtual Address: {X}\n", .{address}),
         };
     }
 
-    // fn tlbTranslate(self: *Cpu, vAddr: u64) u64 {
-    //     const cp0Index = self.cp0.registers[]
-    //     for (self.TLBEntries) |entry| {
-    //         if (!entry.entry_hi.g or entry.entry_hi.vpn2 != (vAddr >> 13)) {
-    //             continue;
-    //         }
-    //
-    //
-    //
-    //         switch (entry.pagemask.mask) {
-    //             0x000 => {
-    //                 // 4KB Page
-    //             },
-    //             0x003 => {
-    //                 // 16KB Page
-    //             },
-    //             0x00F => {
-    //                 // 64KB Page
-    //             },
-    //             0x03F => {
-    //                 // 256KB Page
-    //             },
-    //             0x0FF => {
-    //                 // 1MB Page
-    //             },
-    //             0x03FF => {
-    //                 // 4MB Page
-    //             },
-    //             0x0FFF => {
-    //                 // 16MB Page
-    //             },
-    //             else => {
-    //                 panic("Undefined page size");
-    //             },
-    //         }
-    //     }
-    // }
+    fn tlbTranslate(self: *Cpu, vAddr: u64) u64 {
+        const asid = self.cp0.registers[10] & 0xFF;
+        for (self.TLBEntries) |entry| {
+            // Either ASID has to match or  global bit set
+            if (!entry.entry_hi.g and entry.entry_hi.asid != asid) {
+                continue;
+            }
+
+            // const bitCount = @popCount(entry.pagemask.mask);
+            // const mask = (0xFFFFFFE >> bitCount) << (12 + bitCount);
+            //
+            // // VPN Comparison
+            // if (entry.entry_hi.vpn2 != (vAddr & mask)) {
+            //     continue;
+            // }
+
+            const vpnMask = 0xFFFE0000 | @as(u32, @intCast(~entry.pagemask.mask));
+
+            if (entry.entry_hi.vpn2 & vpnMask != (vAddr >> 13) & vpnMask) {
+                continue;
+            }
+
+            // R Comparison
+            if (entry.entry_hi.r != (vAddr >> 62)) {
+                continue;
+            }
+
+            const pageSize = (@as(u24, entry.pagemask.mask) << 12 | 0x0FFF) + 1;
+            var entryLo: Entry_lo = undefined;
+            if (vAddr & pageSize == 0) {
+                entryLo = entry.entry_lo0;
+            } else {
+                entryLo = entry.entry_lo1;
+            }
+
+            if (entryLo.v == 1) {
+                continue;
+            }
+
+            return entryLo.pfn << 6 | (vAddr & (@as(u24, entry.pagemask.mask) << 12 | 0xFFF));
+        }
+        return 0;
+    }
 
     pub fn emulatorLoop(self: *Cpu) void {
         print("PC: {X}\n", .{self.PC});
